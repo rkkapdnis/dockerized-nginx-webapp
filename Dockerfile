4758a375625ad515442bf4dd30c3017e89443ec7
Rupali K <rkkapdnis1996@gmail.com>
2025-06-25 00:17:32 +0530

    new commit

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,11 @@
 FROM nginx:alpine
 
+# Copy custom entrypoint and template
 COPY entrypoint.sh /entrypoint.sh
 COPY index.html.template /index.html.template
 
+# Give execute permissions to the script
 RUN chmod +x /entrypoint.sh
 
+# Set entrypoint to the script that will generate index.html at runtime
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+# Example IP address substitution (use env var or command)
 IP_ADDR=$(hostname -i)
 
-sed "s/{{IP_ADDRESS}}/$IP_ADDR/" /inedx.html.template > /usr/share/nginx/html/index.html
+# Replace placeholder with actual IP
+envsubst '${IP_ADDR}' < /index.html.template > /usr/share/nginx/html/index.html
 
-nginx -g "daemon off;"
+# Start Nginx
+exec nginx -g 'daemon off;'
